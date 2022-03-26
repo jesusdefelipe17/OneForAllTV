c5d00fd2d70d5b8b3a5558f2e4b3870d4dbfb3aa
jesusdefelipe17 <jesusdefelipe17@gmail.com>
2022-03-26 18:14:52 +0100

    Commit final

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'AllForOne',
+  appName: 'OneForAll TV',
   webDir: 'www',
   bundledWebRuntime: false
 };
